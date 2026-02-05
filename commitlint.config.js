f1e6fa483607e6fe68f7a495161e2ba42ec37914
mindx-devOps <mindx.devops@gmail.com>
2026-02-05 22:53:16 +0530

    feat: dev update

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,16 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'ci']
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'ci'
+      ]
     ],
     'subject-max-length': [2, 'always', 50]
   }
